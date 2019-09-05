cdb74234bc3467fbb3b31cab7130cda934d4d3e6
elring89 <elring2004@mail.ru>
2019-09-06 00:11:59 +0500

    123

--- airports.py
+++ airports.py
@@ -2,67 +2,81 @@ from datetime import date, timedelta
 from time import sleep
 import requests
 from lxml import html
+from geopy.geocoders import Nominatim
+#import wikipedia
 
 MAX_PAGE_COUNT = 3
 SLEEP_SEC_COUNT = 2
 
-city_set = set()
-dt_today = date.today()
-
-
-def get_city_set(days_count=7, page_count=2):
-  """
-    Получение данных из уфимского аэропорта
-  """
-  for d in range(days_count):
-    dt = dt_today + timedelta(days=d)
-    for p in range(1, page_count + 1):
-      url = 'http://www.airportufa.ru/passazhiram/raspisanie.html?filter=1&type=1&flightNumber=0&date={0}.{1}.{2}&city=0&aircompany=0&page={3}'.format(
-        dt.day, dt.month, dt.year, p
-      )
-      r = requests.get(url)
-      tree = html.fromstring(r.text)
-      el = tree.xpath('//div[@class="season-shedule_body"]/div[@class="row collapse"]/div[@class="large-3 columns"]/text()')
-      for i in range(0, len(el), 2):
-        # сначала город, потом авиакомпания (пропускаем)
-        city_set.add(el[i])
-    sleep(SLEEP_SEC_COUNT)
-  return city_set
-
-def print_airports_info_from_wiki(city_set):
-  """
-    Получение краткого описания аэропортов с вики
-  """
-  import wikipedia
-  wikipedia.set_lang("ru")
-  for city in city_set:
-    search_text = '{0} аэропорт'.format(city)
-    print(wikipedia.summary(search_text)) #, sentences=1)
-    print('-----')
-
-def print_city_info(city_set):
-  """
-    Получение информации о городах
-  """
-  from geopy.geocoders import Nominatim
-  geolocator = Nominatim(user_agent="elring")
-  for city in city_set:
-    city_name = city.split('(')[0].strip()
-    print(geolocator.geocode(city_name, language='ru'))
-    print('-----')
-
 city_set = {
-    'КАЗАНЬ', 'СИМФЕРОПОЛЬ', 'ЛАРНАКА', 'ЭНФИДА', 'МОСКВА(Внуково)', 'РОСТОВ-НА-ДОНУ (ПЛАТОВ)', 'ВЕНА',
-    'ХУДЖАНД', 'ЕКАТЕРИНБУРГ (Кольцово)', 'НАРЬЯН-МАР', 'ГАЗИПАША', 'АНТАЛЬЯ', 'НОВОСИБИРСК (Толмачево)',
-    'НАДЫМ', 'СОЧИ', 'ТАЛАКАН', 'МОСКВА (Шереметьево)', 'НИЖНИЙ НОВГОРОД','ЯМБУРГ', 'НИЖНЕВАРТОВСК',
-    'НОВЫЙ УРЕНГОЙ', 'СТАМБУЛ (Стамбул Нью)', 'НОРИЛЬСК', 'САНЬЯ', 'БОВАНЕНКОВО', 'ДУШАНБЕ', 'КОГАЛЫМ',
-    'БАРСЕЛОНА', 'СУРГУТ', 'АНАПА', 'БАКУ', 'ИЖЕВСК', 'СЫКТЫВКАР', 'ДАЛАМАН', 'КРАСНОДАР', 'ГЕЛЕНДЖИК',
-    'ИГАРКА', 'САНКТ-ПЕТЕРБУРГ (Пулково)', 'КРАСНОЯРСК', 'МОСКВА (Домодедово)', 'ИРАКЛИОН', 'ТБИЛИСИ',
-    'ТЮМЕНЬ', 'САЛЕХАРД', 'САМАРА', 'НОЯБРЬСК', 'УСИНСК', 'ПЕРМЬ', 'ОМСК', 'ХАНТЫ-МАНСИЙСК'
+  'КАЗАНЬ', 'СИМФЕРОПОЛЬ', 'ЛАРНАКА', 'ЭНФИДА', 'МОСКВА(Внуково)',
+  'РОСТОВ-НА-ДОНУ (ПЛАТОВ)', 'ВЕНА',
+  'ХУДЖАНД', 'ЕКАТЕРИНБУРГ (Кольцово)', 'НАРЬЯН-МАР',
+  'ГАЗИПАША', 'АНТАЛЬЯ', 'НОВОСИБИРСК (Толмачево)',
+  'НАДЫМ', 'СОЧИ', 'ТАЛАКАН', 'МОСКВА (Шереметьево)',
+  'НИЖНИЙ НОВГОРОД', 'ЯМБУРГ', 'НИЖНЕВАРТОВСК',
+  'НОВЫЙ УРЕНГОЙ', 'СТАМБУЛ (Стамбул Нью)', 'НОРИЛЬСК',
+  'САНЬЯ', 'БОВАНЕНКОВО', 'ДУШАНБЕ', 'КОГАЛЫМ',
+  'БАРСЕЛОНА', 'СУРГУТ', 'АНАПА', 'БАКУ', 'ИЖЕВСК',
+  'СЫКТЫВКАР', 'ДАЛАМАН', 'КРАСНОДАР', 'ГЕЛЕНДЖИК',
+  'ИГАРКА', 'САНКТ-ПЕТЕРБУРГ (Пулково)', 'КРАСНОЯРСК',
+  'МОСКВА (Домодедово)', 'ИРАКЛИОН', 'ТБИЛИСИ',
+  'ТЮМЕНЬ', 'САЛЕХАРД', 'САМАРА', 'НОЯБРЬСК', 'УСИНСК',
+  'ПЕРМЬ', 'ОМСК', 'ХАНТЫ-МАНСИЙСК'
 }
+city_info_dict = {}
 
 
-print_city_info(city_set)
+def fill_city_set(days_count=7, page_count=2):
+    """
+    Получение данных из уфимского аэропорта
+    """
+    print('Запрос в аэропорт')
+    today = date.today()
+    city_set = set()
+    for d in range(days_count):
+        dt = today + timedelta(days=d)
+        for p in range(1, page_count + 1):
+            url = 'http://www.airportufa.ru/passazhiram/raspisanie.html?filter=1&type=1&flightNumber=0&date={0}.{1}.{2}&city=0&aircompany=0&page={3}'.format(
+                dt.day, dt.month, dt.year, p
+            )
+            r = requests.get(url)
+            tree = html.fromstring(r.text)
+            el = tree.xpath('//div[@class="season-shedule_body"]/div[@class="row collapse"]/div[@class="large-3 columns"]/text()')
+            for i in range(0, len(el), 2):
+                # сначала город, потом авиакомпания (пропускаем)
+                city_set.add(el[i])
+        sleep(SLEEP_SEC_COUNT)
 
 
+# def get_airports_info_from_wiki():
+#     """
+#     Получение краткого описания аэропортов с вики
+#     """
+#     if not city_set:
+#         fill_city_set()
+#     wikipedia.set_lang("ru")
+#     for city in city_set:
+#         search_text = '{0} аэропорт'.format(city)
+#         print(wikipedia.summary(search_text))
 
+
+def get_city_info():
+    """
+    Получение информации о городах
+    """
+    if not city_set:
+        fill_city_set()
+    geolocator = Nominatim(user_agent="elring")
+    for city in city_set:
+        city_name = city.split('(')[0].strip()
+        if not city_info_dict.get(city_name):
+            city_info = (geolocator.geocode(city_name, language='ru'))
+            city_info_dict[city_name] = city_info
+    return city_info_dict.values()
+
+
+def get_cities():
+    if not city_set:
+        fill_city_set()
+    return city_set
--- bot.py
+++ bot.py
@@ -1,107 +1,50 @@
 import os
-import requests
-import datetime
 from time import sleep
 import telebot
+from airports import get_city_info, get_cities
 
-'''
-class BotHandler:
-    
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        print('result_json = {0}'.format(result_json))
-        return result_json
-
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
-
-
-def main():
-    now = datetime.datetime.now()
-    new_offset = None
-    today = now.day
-    hour = now.hour
-    token = os.environ.get('TOKEN', '')
-    greet_bot = BotHandler(token)  
-    greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
-    
-
-    while True:
-        print('робот запущен')
-        greet_bot.get_updates(new_offset)
-
-        last_update = greet_bot.get_last_update()
-
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
-
-        new_offset = last_update_id + 1
-'''
 
 token = os.environ.get('TOKEN', '')
-bot = telebot.TeleBot(token)
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Привет', 'Пока')
+bot = telebot.TeleBot(token, threaded=False)
+keyboard = None
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+    bot.send_message(
+        message.chat.id,
+        'Трям! Узнать что летает из Уфы...',
+        reply_markup=keyboard
+    )
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text.lower() == 'я тебя люблю':
-        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+    if message.text.lower() == 'Города':
+        msgs = ', '.join(get_cities())
+        bot.send_message(message.chat.id, msgs)
+    elif message.text.lower() == 'Описание':
+        msgs = ', '.join(get_city_info())
+        bot.send_message(message.chat.id, msgs)
+    print('Обработка сообщения')
 
 
-@bot.message_handler(content_types=['sticker'])
-def sticker_id(message):
-    print(message)
+def init_keyboard():
+    telebot.types.ReplyKeyboardMarkup()
+    keyboard.row('Города', 'Описание')
 
 
 def main():
-    bot.polling()
+    init_keyboard()
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print(e)
+            sleep(15)
 
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
