a7ad8871b2860c64f1ffe4115667c46d4dc95abf
elring89 <elring2004@mail.ru>
2019-09-07 01:20:43 +0500

    123

--- bot.py
+++ bot.py
@@ -2,10 +2,12 @@ import os
 from time import sleep
 import telebot
 import airports
+from flask import Flask, request
 
-bot = telebot.TeleBot(os.environ.get('TOKEN', ''), threaded=False)
+TOKEN = os.environ.get('TOKEN', '')
+bot = telebot.TeleBot(TOKEN, threaded=False)
 airport = airports.UfaAirport()
-
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -22,20 +24,34 @@ def start_message(message):
 def send_text(message):
     print('Обработка сообщения..')
     if message.text.lower() == 'аэропорт уфы на завтра':
-        schedule = airport.get_schedule_msg()
+        schedule = ''#airport.get_schedule_msg()
         msg = schedule or 'Не удалось получить информацию.'
         bot.send_message(message.chat.id, msg)
     print('Обработка сообщения закончена')
 
 
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://ufaflightbot.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
 def main():
     print('Запуск..')
-    while True:
-        try:
-            bot.polling(none_stop=True)
-        except Exception as e:
-            print(e)
-            sleep(15)
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    #while True:
+    #    try:
+    #        bot.polling(none_stop=True)
+    #    except Exception as e:
+    #        print(e)
+    #        sleep(15)
 
 
 if __name__ == '__main__':
